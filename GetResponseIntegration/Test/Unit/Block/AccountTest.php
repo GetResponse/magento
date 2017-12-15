982ff8617cc42ce34754baac2b942ff1b16f6cdf
Radosław Karkut <radoslaw.karkut@getresponse.com>
2017-12-15 10:45:49 +0100

    tests

---
+++ GetResponseIntegration/Test/Unit/Block/AccountTest.php
@@ -0,0 +1,21 @@
+<?php
+namespace GetResponse\GetResponseIntegration\Test\Unit\Block;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class AccountTest
+ * @package GetResponse\GetResponseIntegration\Test\Unit\Block
+ */
+class AccountTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function shouldReturnAccountInfo()
+    {
+//        $repository = $this->getMockBuilder(Repository::class)->getMock();
+
+        self::assertEquals(true, true);
+    }
+}
