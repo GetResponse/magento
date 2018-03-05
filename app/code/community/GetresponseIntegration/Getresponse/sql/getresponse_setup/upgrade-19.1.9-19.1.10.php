7391e13bf986696d7b4b3006cb742e7c65bb9a3a
Radosław Karkut <radoslaw.karkut@getresponse.com>
2018-03-05 12:54:06 +0100

    cleanup in installer

--- app/code/community/GetresponseIntegration/Getresponse/sql/getresponse_setup/upgrade-19.1.9-19.1.10.php
+++ app/code/community/GetresponseIntegration/Getresponse/sql/getresponse_setup/upgrade-19.1.9-19.1.10.php
@@ -7,7 +7,6 @@ $installer = $this;
 $installer->startSetup();
 
 try {
-
     $installer->run("
       CREATE TABLE IF NOT EXISTS `{$installer->getTable('getresponse_product_map')}` (
         `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
@@ -18,17 +17,30 @@ try {
         KEY `entity_id` (`entity_id`),
         CONSTRAINT `getresponse_product_map_ibfk_1` FOREIGN KEY (`entity_id`) REFERENCES `catalog_product_entity` (`entity_id`) ON DELETE CASCADE ON UPDATE CASCADE
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8");
+} catch (\Exception $e) {
+    Mage::helper('getresponse/logger')->logException($e);
+}
 
+try {
     $installer->run("
         ALTER TABLE `{$installer->getTable('sales_flat_order')}` ADD `getresponse_order_id` VARCHAR(32) DEFAULT NULL;
+  ");
+} catch (\Exception $e) {
+    Mage::helper('getresponse/logger')->logException($e);
+}
+
+try {
+    $installer->run("
         ALTER TABLE `{$installer->getTable('sales_flat_order')}` ADD `getresponse_order_md5` VARCHAR(64) DEFAULT NULL;
+  ");
+} catch (\Exception $e) {
+    Mage::helper('getresponse/logger')->logException($e);
+}
+
+try {
+    $installer->run("
         ALTER TABLE `{$installer->getTable('sales_flat_quote')}` ADD `getresponse_cart_id` VARCHAR(32) DEFAULT NULL;
   ");
-
-    $installer->run(
-        "alter table getresponse_settings add column newsletter_cycle_day int after newsletter_campaign_id"
-    );
-
 } catch (\Exception $e) {
     Mage::helper('getresponse/logger')->logException($e);
 }
