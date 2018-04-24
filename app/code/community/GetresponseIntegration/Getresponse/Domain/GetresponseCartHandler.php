fa43132f1924026aed4c69f56070a078f14b0e24
Radosław Karkut <radoslaw.karkut@getresponse.com>
2018-04-24 13:51:20 +0200

    fix in code

--- app/code/community/GetresponseIntegration/Getresponse/Domain/GetresponseCartHandler.php
+++ app/code/community/GetresponseIntegration/Getresponse/Domain/GetresponseCartHandler.php
@@ -67,31 +67,34 @@ class GetresponseIntegration_Getresponse_Domain_GetresponseCartHandler
 
             /** @var Mage_Sales_Model_Quote_Item $product */
             foreach ($quote->getAllVisibleItems() as $product) {
-                $grProducts[$product->getProduct()->getId()] = $this->productHandler->upsertGetresponseProduct($product->getProduct(), $storeId);
-
-                $params = $this->cartBuilder->buildGetresponseCart(
-                    $subscriber['contactId'],
-                    $quote,
-                    $grProducts
+                $grProducts[$product->getProduct()->getId()]
+                    = $this->productHandler->upsertGetresponseProduct(
+                    $product->getProduct(), $storeId
                 );
-
-                /** @var Mage_Sales_Model_Quote $quote */
-                $quote = $this->quoteModel->setStoreId($quote->getStoreId())->load($quote->getId());
-                $grCartId = $quote->getData('getresponse_cart_id');
-
-                if( !empty($grCartId) ) {
-                    $cart = $this->api->updateCart($storeId, $grCartId, $params);
-                } else {
-                    $cart = $this->api->addCart($storeId, $params);
-                }
-                if (!isset($cart['cartId'])) {
-                    return null;
-                }
-
-                $quote->setData('getresponse_cart_id', $cart['cartId']);
-                $quote->save();
-                return $cart['cartId'];
             }
+
+            $params = $this->cartBuilder->buildGetresponseCart(
+                $subscriber['contactId'],
+                $quote,
+                $grProducts
+            );
+
+            /** @var Mage_Sales_Model_Quote $quote */
+            $quote = $this->quoteModel->setStoreId($quote->getStoreId())->load($quote->getId());
+            $grCartId = $quote->getData('getresponse_cart_id');
+
+            if( !empty($grCartId) ) {
+                $cart = $this->api->updateCart($storeId, $grCartId, $params);
+            } else {
+                $cart = $this->api->addCart($storeId, $params);
+            }
+            if (!isset($cart['cartId'])) {
+                return null;
+            }
+
+            $quote->setData('getresponse_cart_id', $cart['cartId']);
+            $quote->save();
+            return $cart['cartId'];
 
         } catch (Exception $e) {
             GetresponseIntegration_Getresponse_Helper_Logger::logException($e);
