a40efbcbe617dea2ed454944a57ec4fa9b82808c
bstepien <bstepien@getresponse.com>
2018-12-12 11:12:22 +0100

    Post Merge master to 2.0

--- Block/Header.php
+++ Block/Header.php
@@ -3,7 +3,6 @@ namespace GetResponse\GetResponseIntegration\Block;
 
 use GetResponse\GetResponseIntegration\Domain\Magento\Repository;
 use GetResponse\GetResponseIntegration\Domain\Magento\WebEventTrackingSettingsFactory;
-use Magento\Customer\Model\Session;
 use Magento\Framework\View\Element\Template;
 use Magento\Framework\View\Element\Template\Context;
 
@@ -16,45 +15,14 @@ class Header extends Template
     /** @var Repository */
     private $repository;
 
-    /** @var Session */
-    private $customerSession;
-
     /**
      * @param Context $context
      * @param Repository $repository
-     * @param Session $customerSession
      */
-    public function __construct(Context $context, Repository $repository, Session $customerSession)
+    public function __construct(Context $context, Repository $repository)
     {
         parent::__construct($context);
         $this->repository = $repository;
-        $this->customerSession = $customerSession;
-    }
-
-    /**
-     * @return array
-     */
-    public function getTrackingData()
-    {
-        $trackingCodeSnippet = $this->getTrackingCodeSnippet();
-
-        return [
-            'isTrackingCodeEnabled' => !empty($trackingCodeSnippet),
-            'trackingCodeSnippet' => $trackingCodeSnippet,
-            'customerEmail' => $this->getLoggedInCustomerEmail(),
-        ];
-    }
-
-    /**
-     * @return string
-     */
-    private function getLoggedInCustomerEmail()
-    {
-        if (false === $this->customerSession->isLoggedIn()) {
-            return '';
-        }
-
-        return $this->customerSession->getCustomer()->getEmail();
     }
 
     /**
