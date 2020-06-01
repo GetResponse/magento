53add6fa91e209dfc4a5bebbcc383ad25af389df
Radosław Karkut <radoslaw.karkut@getresponse.com>
2020-06-01 13:42:28 +0200

    fix for store id

--- Observer/AdminCheckAuthorization.php
+++ Observer/AdminCheckAuthorization.php
@@ -7,6 +7,7 @@ namespace GetResponse\GetResponseIntegration\Observer;
 use GetResponse\GetResponseIntegration\Domain\GetResponse\Account\ReadModel\AccountReadModel;
 use GetResponse\GetResponseIntegration\Domain\SharedKernel\Scope;
 use GetResponse\GetResponseIntegration\Helper\Config;
+use GetResponse\GetResponseIntegration\Helper\MagentoStore;
 use GetResponse\GetResponseIntegration\Helper\Message;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\ActionFlag;
@@ -14,33 +15,32 @@ use Magento\Framework\Event\Observer as EventObserver;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Message\ManagerInterface;
 use Magento\Framework\UrlInterface;
-use Magento\Store\Model\StoreManagerInterface;
 
 class AdminCheckAuthorization implements ObserverInterface
 {
     private $urlInterface;
     private $messageManager;
     private $actionFlag;
-    private $storeManager;
     private $accountReadModel;
+    private $magentoStore;
 
     public function __construct(
         UrlInterface $urlInterface,
         ManagerInterface $messageManager,
         ActionFlag $actionFlag,
-        StoreManagerInterface $storeManager,
-        AccountReadModel $accountReadModel
+        AccountReadModel $accountReadModel,
+        MagentoStore $magentoStore
     ) {
         $this->urlInterface = $urlInterface;
         $this->messageManager = $messageManager;
         $this->actionFlag = $actionFlag;
-        $this->storeManager = $storeManager;
         $this->accountReadModel = $accountReadModel;
+        $this->magentoStore = $magentoStore;
     }
 
     public function execute(EventObserver $observer): AdminCheckAuthorization
     {
-        $scopeId = $this->storeManager->getStore()->getId();
+        $scopeId = $this->magentoStore->getStoreIdFromUrl();
 
         if ($this->isCurrentUrlWhitelisted()) {
             return $this;
