e7b4745d383b6c927c35ad88e3a939821c40dd1b
bstepien <bstepien@getresponse.com>
2021-05-10 13:39:55 +0200

    MAGENTO2-99 - replaced SubscriberManager with Subscriber

--- Observer/CustomerRegisterSuccess.php
+++ Observer/CustomerRegisterSuccess.php
@@ -5,10 +5,6 @@ declare(strict_types=1);
 namespace GetResponse\GetResponseIntegration\Observer;
 
 use Exception;
-use GetResponse\GetResponseIntegration\Api\ApiService;
-use GetResponse\GetResponseIntegration\Domain\GetResponse\Contact\Application\ContactService;
-use GetResponse\GetResponseIntegration\Domain\GetResponse\Contact\ContactCustomFieldsCollectionFactory;
-use GetResponse\GetResponseIntegration\Domain\GetResponse\SubscribeViaRegistration\SubscribeViaRegistrationService;
 use GetResponse\GetResponseIntegration\Domain\Magento\LiveSynchronization;
 use GetResponse\GetResponseIntegration\Domain\Magento\PluginMode;
 use GetResponse\GetResponseIntegration\Domain\Magento\Repository;
@@ -18,27 +14,27 @@ use Magento\Framework\App\RequestInterface;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Newsletter\Model\Subscriber;
-use Magento\Newsletter\Model\SubscriptionManager;
+use Magento\Newsletter\Model\Subscriber as MagentoSubscriber;
 
 class CustomerRegisterSuccess implements ObserverInterface
 {
     private $request;
-    private $subscriptionManager;
+    private $magentoSubscriber;
     private $magentoStore;
     private $logger;
     private $repository;
 
     public function __construct(
         RequestInterface $request,
-        SubscriptionManager $subscriptionManager,
+        MagentoSubscriber $magentoSubscriber,
         MagentoStore $magentoStore,
         Repository $repository,
         Logger $logger
     ) {
         $this->request = $request;
-        $this->repository = $repository;
-        $this->subscriptionManager = $subscriptionManager;
+        $this->magentoSubscriber = $magentoSubscriber;
         $this->magentoStore = $magentoStore;
+        $this->repository = $repository;
         $this->logger = $logger;
     }
 
@@ -62,7 +58,7 @@ class CustomerRegisterSuccess implements ObserverInterface
             $subscriptionOption = $this->request->getParam('is_subscribed');
             if ((int)$subscriptionOption === Subscriber::STATUS_SUBSCRIBED) {
                 $customerId = (int)$observer->getCustomer()->getId();
-                $this->subscriptionManager->subscribeCustomer($customerId, (int)$scope->getScopeId());
+                $this->magentoSubscriber->subscribeCustomerById($customerId);
             }
         } catch (Exception $e) {
             $this->logger->addError($e->getMessage(), ['exception' => $e]);
