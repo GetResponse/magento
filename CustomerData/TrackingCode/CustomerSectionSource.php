0e25e0ab11db52b6f4eee2c409da3071d9a7a4da
Bartłomiej Stępień <bstepien@getresponse.com>
2025-10-03 01:56:25 +0200

    GOLF-446 - adjust Hyvä web events integration with improved code structure and removed unused elements

--- CustomerData/TrackingCode/CustomerSectionSource.php
+++ CustomerData/TrackingCode/CustomerSectionSource.php
@@ -8,46 +8,31 @@ use GetResponse\GetResponseIntegration\Domain\GetResponse\TrackingCode\TrackingC
 use Magento\Customer\CustomerData\SectionSourceInterface;
 use Magento\Customer\Helper\Session\CurrentCustomer;
 use Magento\Framework\Serialize\SerializerInterface;
-use Magento\Framework\App\Helper\AbstractHelper;
-use Magento\Framework\App\Helper\Context;
-use Magento\Framework\View\DesignInterface;
-use Magento\Store\Model\StoreManagerInterface;
 
 class CustomerSectionSource implements SectionSourceInterface
 {
     protected $currentCustomer;
-    protected $storeManager;
-    protected $design;
     private $service;
     private $serializer;
 
     public function __construct(
         CurrentCustomer $currentCustomer,
         TrackingCodeBufferService $service,
-        SerializerInterface $serializer,
-        DesignInterface $design,
-        StoreManagerInterface $storeManager,
+        SerializerInterface $serializer
     ) {
         $this->currentCustomer = $currentCustomer;
         $this->service = $service;
         $this->serializer = $serializer;
-        $this->design = $design;
-        $this->storeManager = $storeManager;
     }
 
     public function getSectionData(): array
     {
-        $theme = $this->design->getDesignTheme();
-        $code = $theme->getCode();
         $email = $this->currentCustomer->getCustomerId() ? $this->currentCustomer->getCustomer()->getEmail() : null;
         $serializedCart = $this->serializer->serialize($this->service->getCartFromBuffer());
-        $currency = $this->storeManager->getStore()->getCurrentCurrency()->getCode();
 
         return [
             'customerEmail' => $email,
-            'cart' => $serializedCart,
-            'theme' => $code,
-            'currency' => $currency
+            'cart' => $serializedCart
         ];
     }
 }
