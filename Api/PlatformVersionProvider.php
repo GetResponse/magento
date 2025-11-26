1f143fcb0296e720cd3710877315611615555342
Adam Skorupka <adam.skorupka@getresponse.com>
2025-11-26 15:32:22 +0100

    GRSD-45502: Add platform/PHP/plugin version headers to http requests

---
+++ Api/PlatformVersionProvider.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GetResponse\GetResponseIntegration\Api;
+
+use Magento\Framework\Module\ModuleListInterface;
+use Magento\Framework\App\ProductMetadataInterface;
+
+class PlatformVersionProvider
+{
+    private ModuleListInterface $moduleList;
+    private ProductMetadataInterface $productMetadata;
+
+    public function __construct(
+        ModuleListInterface $moduleList,
+        ProductMetadataInterface $productMetadata,
+    ) {
+        $this->moduleList = $moduleList;
+        $this->productMetadata = $productMetadata;
+    }
+
+    public function getMagentoVersion(): string
+    {
+        try {
+            return (string)$this->productMetadata->getVersion();
+        } catch (\Exception) {
+            return '';
+        }
+    }
+
+    public function getPhpVersion(): string
+    {
+        return PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION;
+    }
+
+    public function getPluginVersion(): string
+    {
+        try {
+            $moduleInfo = $this->moduleList->getOne('GetResponse_GetResponseIntegration');
+            return (string)($moduleInfo['setup_version'] ?? '');
+        } catch (\Exception) {
+            return '';
+        }
+    }
+}
